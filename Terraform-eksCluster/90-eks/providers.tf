78b42bef59b3ee2abec778dd82c08ca1c259c002
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-12-29 12:39:42 +0530

    ffd

--- Terraform-eksCluster/90-eks/providers.tf
+++ Terraform-eksCluster/90-eks/providers.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "6.22.1"
+      version = "6.23"
     }
   }
    backend "s3" {
     bucket    = "kardev-remote-state"
-    key       = "roboshop-dev/eksCluster"
+    key       = "roboshop-dev-eksCluster"
     region    = "us-east-1"
     use_lockfile = true
     encrypt= true
