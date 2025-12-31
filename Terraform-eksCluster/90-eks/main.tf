a7a164b762cf8967e8b11b31b50660612a610a08
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-12-31 15:26:33 +0530

    ffd

--- Terraform-eksCluster/90-eks/main.tf
+++ Terraform-eksCluster/90-eks/main.tf
@@ -1,3 +1,6 @@
+# after upgrade of cluster from aws console , update the cluster version here and 
+# comment or remove the blue node group and remove the taint from green node group to make it active
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"       # Module version
@@ -36,6 +39,9 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_groups = {
     blue = {
+
+      #create = true
+
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.small", "t3.medium","t3.micro"]
@@ -47,6 +53,28 @@ module "eks" {
       min_size     = 2
       max_size     = 10
       desired_size = 2
+    }
+    # blue green deployment strategy. done for upgrading node groups with zero downtime
+    green = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.small", "t3.medium","t3.micro"]
+      iam_role_additional_policies = {
+        efs = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        ebs = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+      # Adding a taint to the nodes in the green node group to prevent scheduling of pods on them until the upgrade is complete
+      taints = {
+       upgrade={
+        key    = "upgrade"
+        value  = "true"
+        effect = "NO_SCHEDULE"
+       }
+      } 
     }
   }
 
